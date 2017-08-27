aca52340990f50678fb10652e983124ef24f132f
mdmdmdmdmd <exe_tmp@gmx.de>
2017-08-27 20:21:11 +0200

    changed a lot of stuff for the new api, added topics, borrowed youtube mirror stuff from doku5 addon

--- addon.py
+++ addon.py
@@ -17,9 +17,13 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-from datetime import date
+from datetime import datetime
+import time
 from string import ascii_lowercase, digits
-from xbmcswift2 import Plugin, xbmc
+import json
+import requests
+import urllib
+from xbmcswift2 import Plugin, xbmc, xbmcgui
 from resources.lib.api import DokuMonsterApi, NetworkError
 
 PER_PAGE = 50
@@ -28,7 +32,7 @@ plugin = Plugin()
 api = DokuMonsterApi(default_count=PER_PAGE)
 
 STRINGS = {
-    'popular_docus': 30001,
+    'topics': 30001,
     'new_docus': 30002,
     'tags': 30003,
     'all': 30004,
@@ -38,12 +42,66 @@ STRINGS = {
 }
 
 
+class DialogSelect(xbmcgui.WindowXMLDialog):
+    def __init__(self, *args, **kwargs):
+        xbmcgui.WindowXMLDialog.__init__(self)
+        self.listing = kwargs.get("listing")
+        self.title = kwargs.get("title")
+        self.totalitems = 0
+        self.result = None
+
+    def autofocus_listitem(self):
+        pass
+
+    def close_dialog(self, cancelled=False):
+        if cancelled:
+            self.result = False
+        else:
+            self.result = self.list_control.getSelectedItem()
+        self.close()
+
+    def onInit(self):
+        self.list_control = self.getControl(6)
+        self.getControl(3).setVisible(False)
+        self.list_control.setEnabled(True)
+        self.list_control.setVisible(True)
+        self.set_cancel_button()
+        self.getControl(5).setVisible(False)
+        self.getControl(1).setLabel(self.title)
+        self.list_control.addItems(self.listing)
+        self.setFocus(self.list_control)
+        self.totalitems = len(self.listing)
+        self.autofocus_listitem()
+
+    def onAction(self, action):
+        if action.getId() in (9, 10, 92, 216, 247, 257, 275, 61467, 61448, ):
+            self.close_dialog(True)
+        if (action.getId() == 7 or action.getId() == 100) and xbmc.getCondVisibility(
+                "Control.HasFocus(3) | Control.HasFocus(6)"):
+            self.close_dialog()
+
+    def onClick(self, controlID):
+        if controlID == 5:
+            self.result = True
+            self.close()
+        else:
+            self.close_dialog(True)
+
+    def set_cancel_button(self):
+        try:
+            self.getControl(7).setLabel(xbmc.getLocalizedString(222))
+            self.getControl(7).setVisible(True)
+            self.getControl(7).setEnabled(True)
+        except Exception:
+            pass
+
+
 @plugin.route('/')
 def show_root():
     items = (
         # FIXME: Reihe
-        {'label': _('popular_docus'), 'path': plugin.url_for(
-            endpoint='show_popular_docus'
+        {'label': _('topics'), 'path': plugin.url_for(
+            endpoint='show_topics'
         )},
         {'label': _('new_docus'), 'path': plugin.url_for(
             endpoint='show_new_docus'
@@ -66,22 +124,12 @@ def show_root():
 
 @plugin.route('/tags/')
 def show_tags():
-    tags = api.get_tags()
-    items = [{
-        'label': '%s [%s]' % (tag.get('name'), tag.get('count')),
-        'path': plugin.url_for(
-            endpoint='show_docus_by_tag',
-            tag=tag['id']
-        )
-    } for tag in tags]
-    return plugin.finish(items)
+    return __finish_paginate('show_tags', api.get_tags, tags=True)
 
 
 @plugin.route('/tags/<tag>/')
 def show_docus_by_tag(tag):
-    return __finish_paginate(
-        'show_docus_by_tag', api.get_docus_by_tag, tag=tag
-    )
+    return __finish_paginate('show_docus_by_tag', api.get_docus_by_tag, tag=tag)
 
 
 @plugin.route('/initals/')
@@ -105,9 +153,9 @@ def show_docus_by_initial(initial):
     )
 
 
-@plugin.route('/popular_docus/')
-def show_popular_docus():
-    return __finish_paginate('show_popular_docus', api.get_popular_docus)
+@plugin.route('/topics/')
+def show_topics():
+    return __finish_paginate('show_topics', api.get_topics, tags=True)
 
 
 @plugin.route('/top_docus/')
@@ -135,24 +183,47 @@ def search_result(query):
     )
 
 
-@plugin.route('/play/<docu_id>')
-def play(docu_id):
-    docu = api.get_docu(docu_id)
-    plugin.log.info(repr(docu))
-    media = docu.get('media', {})
+@plugin.route('/play/<docu_link>')
+def play(docu_link):
+    media = api.get_docu(docu_link)
+    plugin.log.info(repr(media))
     source, media_type = media.get('source'), media.get('type')
     playback_url = None
     if source == 'youtube.com':
-        if media_type == 'video':
-            playback_url = (
-                'plugin://plugin.video.youtube/'
-                '?action=play_video&videoid=%s' % media.get('id')
-            )
-        elif media_type == 'playlist':
-            playback_url = (
-                'plugin://plugin.video.youtube/'
-                '?action=play_all&playlist=%s' % media.get('id')
-            )
+        if requests.head("http://www.youtube.com/oembed?url=http://www.youtube.com/watch?v={0}&format=json".format(
+                media.get('id'))).status_code == 200:
+            if media_type == 'video':
+                playback_url = (
+                    'plugin://plugin.video.youtube/'
+                    '?action=play_video&videoid=%s' % media.get('id')
+                )
+            elif media_type == 'playlist':
+                playback_url = (
+                    'plugin://plugin.video.youtube/'
+                    '?action=play_all&playlist=%s' % media.get('id')
+                )
+        else:
+            results = []
+            title = media.get('title')
+            for media in _youtube_search(urllib.quote_plus(title.encode(encoding='utf-8'))):
+                label = media["label"]
+                label2 = media["plot"]
+                image = ""
+                if media.get('art'):
+                    if media['art'].get('thumb'):
+                        image = (media['art']['thumb'])
+                listitem = xbmcgui.ListItem(label=label, label2=label2, iconImage=image)
+                listitem.setProperty("path", media["file"])
+                results.append(listitem)
+            title = "{0} \"{1}\"".format(_("Select mirror for"), title.encode(encoding='utf-8'))
+            dialog = DialogSelect("DialogSelect.xml", "", listing=results, title=title)
+            time.sleep(0.1)
+            xbmc.executebuiltin("dialog.Close(busydialog)")
+            dialog.doModal()
+            result = dialog.result
+            if not result:
+                return
+            playback_url = result.getProperty("path")
     elif source == 'vimeo.com':
         if media_type == 'video':
             playback_url = (
@@ -167,11 +238,42 @@ def play(docu_id):
         plugin.notify(msg=_('Not Implemented yet'))
 
 
-def __finish_paginate(endpoint, api_func, *args, **kwargs):
+def _youtube_search(query):
+    FIELDS_BASE = ["dateadded", "file", "lastplayed", "plot", "title", "art", "playcount"]
+    FIELDS_FILE = FIELDS_BASE + ["streamdetails", "director", "resume", "runtime"]
+    FIELDS_FILES = FIELDS_FILE + [
+        "plotoutline", "sorttitle", "cast", "votes", "trailer", "year", "country", "studio",
+        "genre", "mpaa", "rating", "tagline", "writer", "originaltitle", "imdbnumber", "premiered", "episode",
+        "showtitle",
+        "firstaired", "watchedepisodes", "duration", "season"]
+    data = {
+        "jsonrpc": "2.0",
+        "method": "Files.GetDirectory",
+        "id": 1,
+        "params": {
+            "properties": FIELDS_FILES,
+            "directory": "plugin://plugin.video.youtube/kodion/search/query/?q={0}".format(query)
+        }
+    }
+    json_response = xbmc.executeJSONRPC(json.dumps(data))
+    json_object = json.loads(json_response.decode('utf-8'))
+    result = []
+    if 'result' in json_object:
+        for key, value in json_object['result'].iteritems():
+            if not key == "limits" and (isinstance(value, list) or isinstance(value, dict)):
+                result = value
+    result = [i for i in result if not i["filetype"] == "directory"]
+    return result
+
+
+def __finish_paginate(endpoint, api_func, tags=False, *args, **kwargs):
     is_update = 'page' in plugin.request.args
     page = plugin.request.args.get('page', ['1'])[0]
     docus, total_count = api_func(*args, page=page, **kwargs)
-    items = __format_docus(docus)
+    if tags:
+        items = __format_tags(docus)
+    else:
+        items = __format_docus(docus)
     if int(page) > 1:
         p = str(int(page) - 1)
         items.insert(0, {
@@ -183,7 +285,7 @@ def __finish_paginate(endpoint, api_func, *args, **kwargs):
                 **kwargs
             )
         })
-    if int(page) * PER_PAGE > int(total_count):
+    if int(page) * PER_PAGE < int(total_count):
         p = str(int(page) + 1)
         items.append({
             'label': '>> Page %s >>' % p,
@@ -206,30 +308,37 @@ def __finish_paginate(endpoint, api_func, *args, **kwargs):
 def __format_docus(docus, skip_playlists=True):
     items = []
     for i, docu in enumerate(docus):
-        if skip_playlists and docu['media']['type'] == 'playlist':
-            continue
-        tagline = 'language: %s | views: %s | comments: %s' % (
-            docu['lang'], docu['views'], docu['comments']
-        )
-        d = date.fromtimestamp(float(docu['online']))
+        try:
+            d = datetime.strptime(docu['online'], '%Y-%m-%dT%H:%M:%S')
+        except TypeError:
+            d = datetime(*(time.strptime(docu['online'], '%Y-%m-%dT%H:%M:%S')[0:6]))
         pub_date = '%02d.%02d.%04d' % (d.day, d.month, d.year)
-        title = u'[COLOR red][%s°][/COLOR] %s' % (docu['fire'], docu['title'])
+        title = u'%s' % docu['title']
         item = {
             'label': title,
-            'icon': docu['thumb'],
+            'label2': docu['description'].replace('\n\r', '[CR]') or '',
             'info': {
                 'count': i + 1,
-                'studio': docu['username'] or '',
-                'genre': docu['tags'] or '',
-                'tagline': tagline,
-                'plot': docu['description'].replace('\n\r', '[CR]') or '',
+                'tagline': docu['description'].replace('\n\r', '[CR]') or '',
                 'date': pub_date
             },
             'path': plugin.url_for(
                 endpoint='play',
-                docu_id=docu['id']
+                docu_link=docu['link']
             ),
             'is_playable': True
+        }
+        items.append(item)
+    return items
+
+
+def __format_tags(tags):
+    items = []
+    for i, tag in enumerate(tags):
+        item = {
+            'label': '%s [%s]' % (tag.get('name'), tag.get('count')),
+            'info': {'count': i + 1},
+            'path': plugin.url_for(endpoint='show_docus_by_tag', tag=tag['id'])
         }
         items.append(item)
     return items
