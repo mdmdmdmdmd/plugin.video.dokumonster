a0eb19a05f617541aa9e799771cb32772eea1212
mdmdmdmdmd <exe_tmp@gmx.de>
2017-08-28 13:46:42 +0200

    added copyright

--- addon.py
+++ addon.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 #     Copyright (C) 2012 Tristan Fischer (sphere@dersphere.de)
+#     Copyright (C) 2017 mdmdmdmdmd
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
